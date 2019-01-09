8aa7afc4af45bdcff6973c2858ca509c2128f464
Kimwoochang <nanenchanga53@mediasol.kr>
2019-01-09 17:53:04 +0900

    시리얼로그 사용방법 찾기 시작

---
+++ Serilog_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace Serilog_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerilogTest logTest = new SerilogTest();
+            logTest.HelloSerilog();
+
+
+        }
+    }
+}
---
+++ Serilog_test/SerilogTest.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog_test
+{
+    class SerilogTest
+    {
+        public void HelloSerilog()
+        {
+            var log = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            log.Information("hello, Serilog!");
+            log.Warning("Goodbye, Serilog.");
+            Log.CloseAndFlush();
+
+            //이렇게 쓸 수 도 있다
+            //using (var log = new LoggerConfiguration().WriteTo.Console().CreateLogger())
+            //{
+            //    log.Information("hello, Serilog!");
+            //    log.Warning("Goodbye, Serilog.");
+            //}
+        }
+    }
+}
